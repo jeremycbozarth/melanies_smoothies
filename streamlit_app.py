f04ba05473fe260ffb7b2be109fd39255eac52b8
jeremycbozarth <bozarth.jeremy.c@gmail.com>
2025-03-28 12:16:15 -0500

    Update streamlit_app.py
    Dataframe instead of raw JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list:
 # New section to display smoothiefruit nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefruit.response.json(), use_container_width=True)
